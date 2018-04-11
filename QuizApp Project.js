e58a66485149b1834ab6992315d3345b9494eea5
Amir Yousef <ayousef@tampabay.rr.com>
2018-04-11 13:37:47 -0400

    V1.0.0

---
+++ QuizApp Project.js
@@ -0,0 +1,67 @@
+Project Overview:
+
+The Quiz App project is a chance for you to combine and practice everything you learned in this section of the course. 
+You will be making your own Android app - taking it from the idea stage to building out the full app. 
+You can share your app with family and friends, as well as with other students in this course.
+
+The goal is to create an educational app that quizzes a user about a certain topic of your choice. 
+We want you to be creative about how you accomplish this. It’s up to you what the quiz questions will be about and how you want to present them to your user. 
+(We recommend about 4 - 10 quiz questions for a reasonable scope of your app).
+
+Why this project?
+At this point, we believe that you’re now ready to create your own app. You will have full ownership over the creative vision, planning, and development of the app. 
+From what we’ve seen in the discussion forums, you each have a wealth of different experiences and unique areas of expertise. We would like to see that shine through in the app that you create.
+
+What will I learn?
+This project is about combining various ideas and skills we’ve been practicing throughout the course. They include:
+Planning your app design before coding.
+Taking an app layout from drawing to XML code.
+Creating, positioning, and styling views.
+Creating interactivity through button clicks and Java code.
+Commenting and documenting your code.
+
+
+
+PROJECT SPECIFICATION:
+
+App contains 4 - 10 questions, including at least one check box, one radio button, and one text entry.
+
+Questions are in a variety of formats such as free text response, checkboxes, and radio buttons.
+
+Checkboxes are only used for questions with multiple right answers. Radio buttons are only used for questions with a single right answer.
+
+App includes a button for the user to submit their answers and receive a score.
+
+The code adheres to all of the following best practices:
+
+Text sizes are defined in sp
+Lengths are defined in dp
+Padding and margin is used appropriately, such that the views are not crammed up against each other.
+
+The app includes at least four of the following Views: TextView, ImageView, Button, Checkbox, EditText, LinearLayout, RelativeLayout, ScrollView, RadioButton, RadioGroup.
+
+If applicable, the app uses nested ViewGroups to reduce the complexity of the layout.
+
+The app gracefully handles displaying all the content on screen when rotated. Either by updating the layout, adding a scrollable feature or some other mechanism that adheres to Android development guidelines.
+
+The code runs without errors.
+
+Each question has a correct answer.
+
+Radio Button Implementation
+
+Any question which uses radio buttons allows only one to be checked at once.
+
+The app contains at least one if/else statement
+
+The grading button displays a toast which accurately displays the results of the quiz.
+
+The grading logic checks each answer correctly. The app accurately calculates the number of correct answers and does not include incorrect answers in the count.
+
+Note: When applicable, in the grading logic remember to check that the correct answers are checked AND the incorrect answers are not checked.
+
+All variables, methods, and resource IDs are descriptively named such that another developer reading the code can easily understand their function.
+
+The code is properly formatted i.e. there are no unnecessary blank lines; there are no unused variables or methods; there is no commented out code.
+The code also has proper indentation when defining variables and methods.
+
